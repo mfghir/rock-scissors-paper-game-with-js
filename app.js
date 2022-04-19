ef2b7aef49d02f026c2cb2e476c7400f977517ed
miss-f-g <fatemeghafari98@gmail.com>
2022-04-19 19:56:53 +0430

    rock-scissors-paper-game-with-js

---
+++ app.js
@@ -0,0 +1,116 @@
+const game = () => {
+  let pScore = 0;
+  let cScore = 0;
+
+  //  start game
+  const startGame = () => {
+    const playBtn = document.querySelector(".intro button");
+    const introScreen = document.querySelector(".intro");
+    const match = document.querySelector(".match");
+
+    playBtn.addEventListener("click", () => {
+      introScreen.classList.add("fadeOut");
+      match.classList.add("fadeIn");
+    });
+  };
+
+  //   play game
+  const platMatch = () => {
+    const options = document.querySelectorAll(".options button");
+    const playerHand = document.querySelector(".player-hand");
+    const computerHand = document.querySelector(".computer-hand");
+
+    const hands = document.querySelectorAll(".hands img");
+
+    hands.forEach((hand) => {
+      hand.addEventListener("animationend", function () {
+        this.style.animation = "";
+      });
+    });
+
+    const computerOptions = ["rock", "scissors", "paper"];
+
+    options.forEach((options) => {
+      options.addEventListener("click", function () {
+        //computer choice
+        const computerNumber = Math.floor(Math.random() * 3);
+        const computerChoice = computerOptions[computerNumber];
+
+        setTimeout(() => {
+          //
+          compareHands(this.textContent, computerChoice);
+          // update pics
+          playerHand.src = `./assets/${this.textContent}.png`;
+          computerHand.src = `./assets/${computerChoice}.png`;
+        }, 2000);
+
+        playerHand.style.animation = "shakePlayer 2s ease";
+        computerHand.style.animation = "shakeComputer 2s ease";
+      });
+    });
+  };
+
+  const updateScore = () => {
+    const playerScore = document.querySelector(".player-score p");
+    const computerScore = document.querySelector(".computer-score p");
+
+    playerScore.textContent = pScore;
+    computerScore.textContent = cScore;
+  };
+
+  const compareHands = (playerChoice, computerChoice) => {
+    const winner = document.querySelector(".winner");
+
+    if (playerChoice === computerChoice) {
+      winner.textContent = "it's a tie";
+      return;
+    }
+
+    if (playerChoice === "rock") {
+      if (computerChoice === "scissors") {
+        winner.textContent = "Player wins";
+        pScore++;
+        updateScore();
+        return;
+      } else {
+        winner.textContent = "Computer wins";
+        cScore++;
+        updateScore();
+        return;
+      }
+    }
+
+    if (playerChoice === "paper") {
+      if (computerChoice === "scissors") {
+        winner.textContent = "Computer wins";
+        cScore++;
+        updateScore();
+        return;
+      } else {
+        winner.textContent = "Player wins";
+        pScore++;
+        updateScore();
+        return;
+      }
+    }
+
+    if (playerChoice === "scissors") {
+      if (computerChoice === "rock") {
+        winner.textContent = "Computer wins";
+        cScore++;
+        updateScore();
+        return;
+      } else {
+        winner.textContent = "Player wins";
+        pScore++;
+        updateScore();
+        return;
+      }
+    }
+  };
+
+  startGame();
+  platMatch();
+};
+
+game();
